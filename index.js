9cb7f96f1981d0174d06828ecf0044f392b116f6
K Makesh Kumar Naidu <59633482+makeshnaidu@users.noreply.github.com>
2020-04-02 14:26:48 -0700

    Adding dependencies and setting up Modules and app varialbles

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+
+/** 
+ * Required External Modules
+*/
+const express = require("express");
+
+const path = require("path");
+
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = 3000;
+ 
+/**
+ * App Configuration
+ */
+
+ 
+/**
+ * Routes Definitions
+ */
+
+ 
+/**
+ * Server Activation
+ */
