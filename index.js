6263217b02ce508f59174add329341905b6d13d7
K Makesh Kumar Naidu <59633482+makeshnaidu@users.noreply.github.com>
2020-04-02 14:39:41 -0700

    Added Layout and index under views and configured it with express.

--- index.js
+++ index.js
@@ -13,17 +13,23 @@ const path = require("path");
  */
 const app = express();
 const port = 3000;
+
  
 /**
  * App Configuration
  */
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
  
 /**
  * Routes Definitions
  */
-
+app.get("/", (req, res)=>{
+    res.status(200).send("Simple Site for Beginners");
+})
  
 /**
  * Server Activation
  */
+
+ app.listen(port, ()=> console.log(`simple site running on port ${port}!`))
